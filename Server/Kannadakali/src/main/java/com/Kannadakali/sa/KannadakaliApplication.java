9e9748a035f25f351af47776c3dc0e0174bad87a
humblefool94 <akanth1994@gmail.com>
2018-05-12 14:13:21 +0530

    server

---
+++ Server/Kannadakali/src/main/java/com/Kannadakali/sa/KannadakaliApplication.java
@@ -0,0 +1,12 @@
+package com.Kannadakali.sa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KannadakaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KannadakaliApplication.class, args);
+	}
+}
