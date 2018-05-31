b6bfe151b2f84354d779d8f3c550e0a9a7935ead
Humblefool94 <akanth1994@gmail.com>
2018-05-31 16:53:10 +0530

    userformsubmit

--- Client/kannadakali/src/app/app.component.ts
+++ Client/kannadakali/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  public userdetails: FormGroup;
+  public emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$';
+  public mobileNumber = '^[789]+[0-9]{9,9}$';
+  public name = '^[A-Za-z]{3,15}$';
+  public years = '^[0-99]{0,2}$';
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.userdetails = this.fb.group({
+      uname: [null, Validators.compose([Validators.required, Validators.pattern(this.name)])],
+      uoriginalplace: [null, Validators.compose([Validators.required, Validators.pattern(this.name)])],
+      uoccupation: [null, Validators.compose([Validators.required, Validators.pattern(this.name)])],
+      ustayduration: [null, Validators.compose([Validators.required , Validators.pattern(this.years)])],
+      uemail: [null, Validators.compose([Validators.required , Validators.pattern(this.emailPattern)])],
+      umobilenumber: [null , Validators.compose([Validators.pattern(this.mobileNumber)])]
+    });
+  }
+
+  userFormSubmit(userdetails) {
+    console.log(userdetails);
+  }
 }
--- Client/kannadakali/src/app/app.module.ts
+++ Client/kannadakali/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AppRoutes } from './app.routing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
-
-@NgModule({
+@NgModule ({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    RouterModule.forRoot(AppRoutes , {useHash: true}),
   ],
   providers: [],
   bootstrap: [AppComponent]
